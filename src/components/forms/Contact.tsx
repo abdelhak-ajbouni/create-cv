0cc3a6e75df713eb59ed3d8e611fcc01c8f96cbb
abdelhak <abdelhak@groupado.com>
2022-01-26 20:43:10 +0100

    convert Contact component to tsx

--- src/components/forms/Contact.tsx
+++ src/components/forms/Contact.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useForm } from "react-hook-form";
-import { Select, MenuItem, Box } from '@mui/material';
 
 import TextField from '../common/TextField';
 import Button from '../common/Button';
@@ -11,22 +10,22 @@ export default function Contact({ }) {
 
   return (
     <form onSubmit={handleSubmit((data) => setResult(data))}>
-      <Box sx={{ display: "flex" }}>
+      <div>
         <TextField register={register} label="Full name" />
         <TextField register={register} label="Email" />
-      </Box>
-      <Box sx={{ display: "flex" }}>
+      </div>
+      <div>
         <TextField register={register} label="Phone number" />
         <TextField register={register} label="Linkedin" />
-      </Box>
-      <Box sx={{ display: "flex" }}>
+      </div>
+      <div>
         <TextField register={register} label="Website" />
-        <Select {...register("category")} label="State">
+        {/* <Select {...register("category")} label="State">
           <MenuItem value="">Tunis</MenuItem>
           <MenuItem value="">Sfax</MenuItem>
-        </Select>
-      </Box>
-      <Button type="submit" loading={false}>Save</Button>
+        </Select> */}
+      </div>
+      <Button loading={true}>Save</Button>
     </form>
   );
 }
