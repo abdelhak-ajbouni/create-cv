659bf94498a8980971fb33f52d3df3ad66750976
abdelhak <abdelhak@groupado.com>
2022-02-12 17:56:06 +0100

    add actions to TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from "next/image"
 import { useSession, signIn, signOut } from "next-auth/react"
 
@@ -21,8 +22,19 @@ export default function TopBar({ }: Props) {
     <div className="top-bar bg-white dark:bg-gray-900 shadow-md shadow-gray-100">
       <Container className="justify-between px-4 py-2">
         <div className="top-bar-wrapper flex justify-between">
-          <Image src={logo} alt='logo' height={40} width={100} />
-          <Button label={`${session ? session?.user?.name : "login"}`} onClick={handleSignIn} />
+          <div className="logo">
+            <Link href="/" passHref>
+              <Image src={logo} alt='logo' height={30} width={100} />
+            </Link>
+          </div>
+          <div className="actions">
+            {session && (
+              <Link href="/builder" passHref >
+                <Button className='mx-8' label="Builder" secondary />
+              </Link>
+            )}
+            <Button label={`${session ? session?.user?.name : "login"}`} onClick={handleSignIn} />
+          </div>
         </div>
       </Container>
     </div >
